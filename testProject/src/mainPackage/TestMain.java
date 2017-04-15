bfaf54775a3547af31a140cff4a41ba5179c1beb
Cyrill <Cyrill@myDesktopPC.home>
2017-04-15 15:48:18 +0200

    First Change

--- testProject/src/mainPackage/TestMain.java
+++ testProject/src/mainPackage/TestMain.java
@@ -5,6 +5,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Das ist ein Test");
+		
+		System.out.println("hier kommt der andere Test, ready to push :)");
 	}
 
 }
