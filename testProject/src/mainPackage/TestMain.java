ff9585b2faa78149a9bbe677ac4c59b3f278e32c
Cyrill <Cyrill@myDesktopPC.home>
2017-04-15 15:40:41 +0200

    Inital chein of my testclass

---
+++ testProject/src/mainPackage/TestMain.java
@@ -0,0 +1,10 @@
+package mainPackage;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Das ist ein Test");
+	}
+
+}
